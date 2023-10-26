bc8cf0bb079a53c5426e636a2a7118418ce8ea74
AwesomeAdil <84029606+AwesomeAdil@users.noreply.github.com>
2023-10-25 22:06:56 -0500

    exponentiation commit

---
+++ exponentiation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+typedef long long ll;
+using namespace std;
+ll MOD = 1000000007;
+ll exp(ll a, ll b){
+  if(b == 0) return 1;
+  ll val = exp(a, b/2)%MOD;
+  val *= val;
+  val%=MOD;
+
+  if(b%2 == 0) return val;
+
+  return (val * a)%MOD;
+}
+
+int main(){
+  int n; cin>>n;
+  for(int i=0;i<n;i++){
+    ll a, b; cin>>a>>b;
+    cout<<exp(a, b)<<endl;
+  }
+
+}
