9fcd4722b75beed89c99ba28b78d022be011d73e
AwesomeAdil <84029606+AwesomeAdil@users.noreply.github.com>
2023-11-02 22:18:53 -0500

    greg and graph comments commit

--- greg_and_graph.cpp
+++ greg_and_graph.cpp
@@ -3,18 +3,29 @@
 typedef long long ll;
 using namespace std;
 
+/*
+ * IDEA: We do the floyd warshals with an ordering (the reverse of the removals)
+ * This way, we get that the last to remove is the equivalent to the first added.
+ * Due to the nature of the floyd warshals, dp[i][j] will represent the min dist
+ * at every dist when including all k nodes seen so far as intermediates. We then
+ * only added allowed nodes to the sum to get the answers.
+ */
+
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   int n; cin>>n;
-  ll adjmat[n][n];
   int order[n];
-  ll ans[n];
   bool allowed[n];
+  ll adjmat[n][n];
+  ll ans[n];
+
+  // Read in adjacency matrix
   for(int i=0;i<n;i++)
     for(int j=0;j<n;j++)
       cin>>adjmat[i][j];
 
+  // Read in and initialize auxilary and answer matricies
   for(int i=0;i<n;i++){
     cin>>order[n-i-1];
     ans[i] = 0;
@@ -22,11 +33,13 @@ int main(){
   }
 
   for(int k=0;k<n;k++){
+    //intermediates allowed and note we account for ordering
     int added = order[k]-1;
     allowed[added] = true;
     for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
         adjmat[i][j] = min(adjmat[i][j], adjmat[i][added] + adjmat[added][j]);
+        // Check if this are nodes in graph
         if (i!=j && allowed[i] && allowed[j])
           ans[n-k-1]+=adjmat[i][j];
       }
